0dcf14d5b2394f32b73688b9b1624347ff86603c
monkey113333 <mgaungkyaemhon5@gmail.com>
2023-05-19 22:06:21 +0630

    day4

---
+++ js/condition.js
@@ -0,0 +1,60 @@
+
+
+
+// {
+//     console.log("a")
+//     console.log("b")
+//     console.log("c")
+// }
+
+// function run(){
+//     console.log("a")
+//     console.log("b")
+//     console.log("c")
+// }
+
+// run()
+
+// if (false) {
+//     console.log("a")
+//     console.log("b")
+//     console.log("c")
+// } else{
+//     console.log("this is false")
+// }
+
+// function canIcome (wakeup){
+//     if (wakeup <=9){
+//         console.log("go to class")
+//     }else{
+//         console.log("don't go to class")
+//     }
+
+// }
+
+// function canIcome (wakeup){
+//     // let result;
+//     // if (wakeup <=9){
+//     //     result = "go to class"
+//     // }else{
+//     //     result = "not to go class"
+//     // }
+
+//     if (wakeup <= 9){return "go to class"}
+
+//     return "not to go class"
+// }
+// console.log(canIcome(8))
+// console.log(canIcome(10))
+// console.log(canIcome(9))
+
+
+let mark = 90;
+
+if(mark >= 80){
+    console.log("Distation")
+}else if(mark >=40){
+    console.log("Pass")
+}else if(mark < 40){
+    console.log("Fail")
+}
